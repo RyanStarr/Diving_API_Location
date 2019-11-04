cb3d993cedbd24e711c011bb569ae880598f4407
ryan <jimmyjones1256@gmail.com>
2019-11-04 17:29:54 +0000

    Connected to database + commented

--- diving.py
+++ diving.py
@@ -1,35 +1,46 @@
+# Libraries for connection to API
 import urllib.request
+# Reading JSON
 import json
+# And storing output in database
+import pymongo
 
-def get_jsonparsed_data(url):
+
+# Perform API GET request
+def get_diving_data(url):
     response = urllib.request.urlopen(url)
-    data = response.read().decode("utf-8")
+    data = response.read().decode("ISO-8859-1")
     return json.loads(data)
 
-def entries_to_remove(entries, the_dict):
-    for key in entries:
-        if key in the_dict:
-            del the_dict[key]
+
 def main():
-    url = 'http://api.divesites.com/?mode=sites&lat=50.3352&lng=-4.1485&dist=25'
-    print("ARRAY: ---", get_jsonparsed_data(url))
-    site_array = get_jsonparsed_data(url)['sites']
-   # print(site_array)
+    # Connect to database
+    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+    mydb = myclient["diving_location_db"]
+    mycol = mydb["location"]
 
-    remove_words = ("currents", "distance", "hazards")
-    #
-    number = 0
-    sites = site_array[0]
-    for location in site_array:
-        number = number +1
-      # print(location)
+    # Define API information
+    url = 'http://api.divesites.com/?mode=sites&lat=50.3352&lng=-4.1485&dist=2500000000000000000000'
 
-        #print("DICT" + str(sites))
-        for i in ["currents", "distance", "hazards", "mindepth", "maxdepth", "equipment", "description", "predive", "marinelife", "water"]:
+    # Retrieve only diving site locations
+    diving_site_array = get_diving_data(url)['sites']
+
+    # Trim useless data from array
+    for location in diving_site_array:
+        for i in ["id", "currents", "distance", "hazards", "mindepth", "maxdepth", "equipment", "description",
+                  "predive", "marinelife", "water"]:
             location.pop(i)
-        for key in sorted(location.keys()):
-            print("%s: %s" % (key, location[key]))
 
-   # entries_to_remove(dict(sites), list(remove_words))
-if __name__=='__main__':
+        # -----Used to view output-----
+        '''
+        for key in (location.keys()):
+            print("%s: %s" % (key, location[key]))
+        '''
+
+        # Output each location to database
+        x = mycol.insert_one(location)
+
+
+# Start program from main()
+if __name__ == '__main__':
     main()
